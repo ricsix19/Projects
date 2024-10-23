c485c471231837f95830424eec9d5cd23dfbfd9a
ricsix19 <159322911+ricsix19@users.noreply.github.com>
2024-10-23 23:30:07 +0200

    timer update

--- random_projects/os_shutdown.py
+++ random_projects/os_shutdown.py
@@ -6,15 +6,27 @@ from tkinter import messagebox
 def shutdown():
     current_os = platform.system()
 
-    if messagebox.askokcancel("Shutdown", "Do you really want to shut down the computer?"):     #ask the user if he wants to shutdown the os, you can take out the first 'if' statement if you dont want a checkbox asking if you really want to shutdown your computer and if you take out the first 'if' statement then you can take out the else statement too.
-        if current_os == "Windows":
-            os.system("shutdown /s /t 1800")    #/s gives the os the command that it wants to shutdown, /t is time in seconds
-        else:
-            messagebox.showerror("Error", "Unsupported OS")
-
+    timer = timer_entry.get()
+    try:
+        timer = int(timer)
+        if messagebox.askokcancel("Shutdown", "Do you really want to shut down the computer?"):     #ask the user if he wants to shutdown the os, you can take out the first 'if' statement if you dont want a checkbox asking if you really want to shutdown your computer and if you take out the first 'if' statement then you can take out the else statement too.
+            if current_os == "Windows":
+                os.system(f"shutdown /s /t {timer}")    #/s gives the os the command that it wants to shutdown, /t is time in seconds
+            else:
+                messagebox.showerror("Error", "Unsupported OS")
+    except ValueError:
+        messagebox.showerror("Invalid number", "Enter valid number")
 #Main window
 app = tk.Tk() #declaring the main window
 app.title("Shutdown GUI")
+
+#label
+label = tk.label(app, text="Enter time in seconds:")
+label.pack(pady=10)
+
+#entry
+timer_entry = tk.Entry(app, width=10)
+timer_entry.pack(pady=5)
 
 #button
 shutdown_button = tk.Button(app, text="Shutdown", command=shutdown, height=2, width=10) #this creates the button, the 'app' is the parent or master class that gets called, the rest is self explanatory
